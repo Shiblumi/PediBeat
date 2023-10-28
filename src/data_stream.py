39cf3ea841a91bd97eb88a120856bcf0cbd1e010
shiblumi <diwilson@ucsc.edu>
2023-10-27 19:16:04 -0700

    Added draft attach and detach functionality to data_stream

--- src/data_stream.py
+++ src/data_stream.py
@@ -1,10 +1,19 @@
 import csv
 
 class data_stream:
-    def __init__(self, csv_file=''):
-        self.csv_file = csv_file
+    
+    def __init__(self, csv_file_path=''):
+        self.buffer1 = []
+        self.buffer2 = []
+        self.buffer3 = []
         
-    def attach(self, csv_file):
+        if (~csv_file_path):
+            self.csv_file_obj = None
+        else:
+            self.csv_file_obj = self.attach(csv_file_path)
+            
+        
+    def attach(self, csv_file_path):
         """
         Attach instance of data_stream to a csv file.
 
@@ -12,10 +21,12 @@ class data_stream:
             csv_file (string): Path to csv file to attach.
 
         Returns:
-            type: Description of the return value.
+            None
         """
-        with open("students.csv") as csv_file: 
-                csv_reader = csv.DictReader(csv_file)
-
-                for row in csv_reader:
-                    pass
+        file = open(csv_file_path)
+        self.csv_file_obj = csv.DictReader(file)
+    
+    
+    def detach(self):
+        self.csv_file_obj.close()
+        
