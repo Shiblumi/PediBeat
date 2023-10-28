99f57dac423435721abecb97d165f97df5f897b6
shiblumi <diwilson@ucsc.edu>
2023-10-27 19:16:48 -0700

    Moved main to /src

--- main.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-if __name__ == 'main':
-
-    with open("students.csv") as csv_file: 
-        csv_reader = csv.DictReader(csv_file)
-
-        for row in csv_reader:
-            pass
