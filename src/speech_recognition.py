a8c49f99069c531e9ddbe16b19ac79f30622360f
shiblumi <diwilson@ucsc.edu>
2023-10-29 07:36:32 -0700

    Wrapper made for speech recognition

--- src/speech_recognition.py
+++ src/speech_recognition.py
@@ -33,3 +33,47 @@ def run_quickstart() -> speech.RecognizeResponse:
 
     for result in response.results:
         print(f"Transcript: {result.alternatives[0].transcript}")
+        
+        
+        
+
+# Source: https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/speech/snippets/quickstart.py
+# Imports the Google Cloud client library
+
+from google.cloud import speech
+import sys
+sys.path.append('data/')
+
+
+class SpeechRecognition:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        
+        
+    def set_file_path(self, file_path: str):
+        self.file_path = file_path
+        
+
+    def run(self) -> speech.RecognizeResponse:
+        # Instantiates a client
+        client = speech.SpeechClient.from_service_account_file("C:/Users/wilso/OneDrive/Documents/Code/keys/analog-daylight-403506-6c4956196627.json")
+
+        # The name of the audio file to transcribe
+        file_name = self.file_path
+
+        with open(file_name, "rb") as audio_file:
+            wav_data = audio_file.read()
+
+        audio = speech.RecognitionAudio(content=wav_data)
+
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=48000,
+            enable_automatic_punctuation=True,
+            language_code="en-US",
+        )
+
+        # Detects speech in the audio file
+        response = client.recognize(config=config, audio=audio)
+
+        return response.results[0].alternatives[0].transcript
