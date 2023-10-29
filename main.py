adf6126f1f125e4f45cd5c61ac74de8b964d619e
shiblumi <diwilson@ucsc.edu>
2023-10-29 06:50:47 -0700

    Created class for OpenAI API interaction

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import sys
+import openai
 sys.path.append('src/')
-import data_stream as ds
+from data_stream import DataStream
+from openAI_convo import OpenAIConvo
+
 
 if __name__ == '__main__':
-    d_stream = ds.DataStream(5, 10, 15)
+    d_stream = DataStream(5, 10, 15)
     d_stream.initialize_csv('col1', 'col2', 'col3')
     for i in range(50):
         d_stream.feed_buffers(i, i, i)
-    d_stream.display()
-    d_stream.flush_all_to_csv()
+    # d_stream.display()
+    d_stream.flush_buffers_to_csv()
+    
+    
+    # Set up OpenAI API key
+    API_KEY = open("C:/Users/wilso/OneDrive/Documents/Code/keys/key_openAI_cal-hacks", "r").read()
+    openai.api_key = API_KEY
+    
+    chatGPT = OpenAIConvo()
+    chatGPT.add_user_content("What is the largest planet in our solar system?")
+    response = chatGPT.run_convo()
+    print(response)
+    
+    chatGPT.add_assistant_content(response)
+    chatGPT.add_user_content("In one sentence, what is the second largest?")
+    response = chatGPT.run_convo()
+    print(response)
+    
     
 # if __name__ == '__main__':
 #     d_stream = ds.DataStream(5, 10, 15, csv_file_path='./data/test.csv', save_rate=10)
--- src/openAI_convo.py
+++ src/openAI_convo.py
@@ -1,41 +1,28 @@
 import openai
 
-API_KEY = open("C:/Users/wilso/OneDrive/Documents/Code/keys/key_openAI_cal-hacks", "r").read()
-openai.api_key = API_KEY
-
-system_content = (f"You are an assistant for a doctor, analyzing a patient's vitals. I need you \
-    to give me a diagnosis of the patient's condition based on this data. The data is in the form \
-    of a csv file.")
-user_content = "What is the third planet from the sun?"
-assistant_content = ''
-
-
-# response = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "system", "content": system_content},
-#         {"role": "user", "content": "PUT YOUR STR VAR HERE"},
-#         {"role": "assistant", "content": "PUT YOUR STR VAR HERE"},
-#     ]
-# )
-
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": user_content},
-    ]
-)
-
-response1 = response['choices'][0]['message']['content']
-print(response1)
-
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "Now, what's the planet next to it?"},
-        {"role": "assistant", "content": response1},
-    ]
-)
-
-response2 = response['choices'][0]['message']['content']
-print(response2)
+class OpenAIConvo:
+    def __init__(self, user_content='', system_content='', assistant_content=''):
+        self.user_content = user_content
+        self.system_content = system_content
+        self.assistant_content = assistant_content
+        
+    def run_convo(self):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": self.system_content},
+                {"role": "user", "content": self.user_content},
+                {"role": "assistant", "content": self.assistant_content}
+            ]
+        )
+        self.assistant_content = response['choices'][0]['message']['content']
+        return self.assistant_content
+    
+    def add_user_content(self, user_content):
+        self.user_content = user_content
+        
+    def add_system_content(self, system_content):
+        self.system_content = system_content
+        
+    def add_assistant_content(self, assistant_content):
+        self.assistant_content = assistant_content
