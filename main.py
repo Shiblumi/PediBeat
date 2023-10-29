2f69393c90baaf98924848fe2c496f2710164626
shiblumi <diwilson@ucsc.edu>
2023-10-29 07:36:51 -0700

    New toy tests

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import openai
 sys.path.append('src/')
 from data_stream import DataStream
 from openAI_convo import OpenAIConvo
+from speech_recognition import SpeechRecognition
 
 
 if __name__ == '__main__':
+    # SpeechRecognition Test
+    sr = SpeechRecognition("C:/Users/wilso/OneDrive/Documents/Code/CalHacks/data/TQBFJOTLD.wav")
+    speech_to_text = sr.run()
+    print(speech_to_text)
+    
+    # DataStream Test
     d_stream = DataStream(5, 10, 15)
     d_stream.initialize_csv('col1', 'col2', 'col3')
     for i in range(50):
@@ -14,23 +21,19 @@ if __name__ == '__main__':
     d_stream.flush_buffers_to_csv()
     
     
-    # Set up OpenAI API key
-    API_KEY = open("C:/Users/wilso/OneDrive/Documents/Code/keys/key_openAI_cal-hacks", "r").read()
-    openai.api_key = API_KEY
+    # print(d_stream.get_last_n_from_csv(5))
     
-    chatGPT = OpenAIConvo()
-    chatGPT.add_user_content("What is the largest planet in our solar system?")
-    response = chatGPT.run_convo()
-    print(response)
+    # # OpenAIConvo Test
+    # # Set up OpenAI API key
+    # API_KEY = open("C:/Users/wilso/OneDrive/Documents/Code/keys/key_openAI_cal-hacks", "r").read()
+    # openai.api_key = API_KEY
     
-    chatGPT.add_assistant_content(response)
-    chatGPT.add_user_content("In one sentence, what is the second largest?")
-    response = chatGPT.run_convo()
-    print(response)
+    # chatGPT = OpenAIConvo()
+    # chatGPT.add_user_content("What is the largest planet in our solar system?")
+    # response = chatGPT.run_convo()
+    # print(response)
     
-    
-# if __name__ == '__main__':
-#     d_stream = ds.DataStream(5, 10, 15, csv_file_path='./data/test.csv', save_rate=10)
-#     for i in range(50):
-#         d_stream.feed_buffers(i, i, i)
-#     d_stream.display()
+    # chatGPT.add_assistant_content(response)
+    # chatGPT.add_user_content("In one sentence, what is the second largest?")
+    # response = chatGPT.run_convo()
+    # print(response)o
